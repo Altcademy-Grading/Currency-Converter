bde22c827593767404200eb5db0cfee8142e1f44
nickstauffer21 <nickstauffer456@gmail.com>
2023-09-07 14:07:14 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./Navbar";
+import List from "./pages/List";
+import Converter from "./pages/Converter";
+import Home from "./pages/Home";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/converter" element={<Converter />}></Route>
+          <Route path="/list" element={<List />}></Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
